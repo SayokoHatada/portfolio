d937c438958b7023de6edc6a919267cdd55cf58e
Sayoko Hatada <sayoko@Sayokononotobukkukonpyuta.local>
2024-11-12 09:44:22 +0900

    マイページのアクセス制限,リダイレクト先修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
   before_action :require_login
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
   private
 
   def not_authenticated
     redirect_to login_path
   end
+
+  def require_login
+    unless current_user
+      redirect_to login_path
+    end
+  end
+
 end
