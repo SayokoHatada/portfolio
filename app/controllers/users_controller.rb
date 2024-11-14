624859d04716e098b46e56e0f78c682a7d60f387
Sayoko Hatada <sayoko@Sayokononotobukkukonpyuta.local>
2024-11-14 10:22:44 +0900

    エラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
       redirect_to login_path, success: "User registration completed"
     else
       flash.now[:danger] = "User registration failed"
-      render :new status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
