519eccdb0bb47b34aa9ab8d0927a3c4bf17c06ec
Sayoko Hatada <sayoko@Sayokononotobukkukonpyuta.local>
2024-11-14 16:33:02 +0900

    エラー修正

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -8,6 +8,7 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal "パスワードリセット", mail.subject
     assert_equal [ user.email ], mail.to
     assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match "User One", mail.body.encoded
+    assert_match edit_password_reset_url(user.reset_password_token), mail.body.encoded
   end
 end
