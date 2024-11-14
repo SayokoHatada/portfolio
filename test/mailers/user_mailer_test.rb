fee705d48eb2e86e85ccaf2f9a960b051bdda3e8
Sayoko Hatada <sayoko@Sayokononotobukkukonpyuta.local>
2024-11-14 16:47:09 +0900

    エラー修正

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,10 +1,11 @@
 require "test_helper"
+include Rails.application.routes.url_helpers 
 
 class UserMailerTest < ActionMailer::TestCase
   test "reset_password_email" do
     user = users(:one)
     user.save
-    mail = UserMailer.reset_password_email(user)
+    mail = UserMailer.reset_password_email(user).deliver_now
 
     assert_equal "パスワードリセット", mail.subject
     assert_equal [ user.email ], mail.to
