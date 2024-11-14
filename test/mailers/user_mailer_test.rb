3e55db03734622035fbb8ab9d74d7922ad0d6039
Sayoko Hatada <sayoko@Sayokononotobukkukonpyuta.local>
2024-11-14 16:39:18 +0900

    エラー修正

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -3,12 +3,13 @@ require "test_helper"
 class UserMailerTest < ActionMailer::TestCase
   test "reset_password_email" do
     user = users(:one)
+    user.save
     mail = UserMailer.reset_password_email(user)
 
     assert_equal "パスワードリセット", mail.subject
     assert_equal [ user.email ], mail.to
     assert_equal [ "from@example.com" ], mail.from
-    assert_match "User One", mail.body.encoded
-    assert_match edit_password_reset_url(user.reset_password_token), mail.body.encoded
+    assert_match "User One", mail.text_part.decoded
+    assert_match edit_password_reset_url(user.reset_password_token), mail.text_part.decoded
   end
 end
